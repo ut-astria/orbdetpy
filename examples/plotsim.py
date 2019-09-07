f39333018444043f619b67045ec659287021c17e
Shiva Iyer <shiva.iyer@utexas.edu>
2019-09-07 11:43:36 -0500

    Added RAAN and perigee argument to simulation plots.

--- examples/plotsim.py
+++ examples/plotsim.py
@@ -30,28 +30,72 @@ def main(args):
         out = json.load(f)
 
     mu = 398600.4418
-    tstamp, hvec, hmag, ener, sma, ecc, inc = [], [], [], [], [], [], []
+    tstamp, hvec, hmag, ener, sma, ecc, inc, raan, argp = [], [], [], [], [], [], [], [], []
     for o in out:
         rv = [x/1000.0 for x in o["TrueState"]["Cartesian"][:6]]
-        r, v = norm(rv[:3]), norm(rv[3:])
+        r = norm(rv[:3])
+        v = norm(rv[3:])
         h = numpy.cross(rv[:3], rv[3:])
         hn = norm(h)
         a = 1.0/(2.0/r-v*v/mu)
-        e = math.sqrt(1 - hn*hn/(mu*a))
+        e = numpy.cross(rv[3:], h)/mu - rv[:3]/r
+        en = norm(e)
         i = math.acos(h[2]/hn)*180.0/math.pi
+        n = numpy.cross([0, 0, 1], h)
+        nn = norm(n)
+        O = math.acos(numpy.dot(n, [1, 0, 0])/nn)*180.0/math.pi
+        if (numpy.dot([0, 1, 0], n) < 0.0):
+            O = 360.0 - O
+        w = math.acos(numpy.dot(n, e)/(nn*en))*180.0/math.pi
+        if (numpy.dot([0, 0, 1], e) < 0.0):
+            w = 360.0 - w
 
         tstamp.append(dateutil.parser.isoparse(o["Time"]))
         hvec.append(h)
         hmag.append(hn)
         ener.append(v**2/2.0 - mu/r)
         sma.append(a)
-        ecc.append(e)
+        ecc.append(en)
         inc.append(i)
+        raan.append(O)
+        argp.append(w)
 
     hvec = numpy.array(hvec)
     tim = [(t - tstamp[0]).total_seconds()/3600 for t in tstamp]
 
-    fig = plt.figure(0)
+    plt.figure(0)
+    plt.subplot(511)
+    plt.plot(tim, sma, "-b")
+    plt.xlabel("Time [hr]")
+    plt.ylabel("SMA [km]")
+    plt.subplot(512)
+    plt.plot(tim, ecc, "-b")
+    plt.xlabel("Time [hr]")
+    plt.ylabel("Eccentricity")
+    plt.subplot(513)
+    plt.plot(tim, inc, "-b")
+    plt.xlabel("Time [hr]")
+    plt.ylabel("Inclination [deg]")
+    plt.subplot(514)
+    plt.plot(tim, raan, "-b")
+    plt.xlabel("Time [hr]")
+    plt.ylabel("RAAN [deg]")
+    plt.subplot(515)
+    plt.plot(tim, argp, "-b")
+    plt.xlabel("Time [hr]")
+    plt.ylabel("Perigee arg. [deg]")
+
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(tim, hmag, "-b")
+    plt.xlabel("Time [hr]")
+    plt.ylabel("Angular momentum")
+    plt.subplot(212)
+    plt.plot(tim, ener, "-b")
+    plt.xlabel("Time [hr]")
+    plt.ylabel("Specific energy")
+
+    plt.figure(2)
     plt.subplot(311)
     plt.plot(tim, hvec[:,0], "-b")
     plt.xlabel("Time [hr]")
@@ -65,30 +109,6 @@ def main(args):
     plt.xlabel("Time [hr]")
     plt.ylabel("h(z)")
     plt.suptitle("Components of angular momentum")
-
-    fig = plt.figure(1)
-    plt.subplot(211)
-    plt.plot(tim, hmag, "-b")
-    plt.xlabel("Time [hr]")
-    plt.ylabel("Angular momentum")
-    plt.subplot(212)
-    plt.plot(tim, ener, "-b")
-    plt.xlabel("Time [hr]")
-    plt.ylabel("Specific energy")
-
-    fig = plt.figure(2)
-    plt.subplot(311)
-    plt.plot(tim, sma, "-b")
-    plt.xlabel("Time [hr]")
-    plt.ylabel("SMA [km]")
-    plt.subplot(312)
-    plt.plot(tim, ecc, "-b")
-    plt.xlabel("Time [hr]")
-    plt.ylabel("Eccentricity")
-    plt.subplot(313)
-    plt.plot(tim, inc, "-b")
-    plt.xlabel("Time [hr]")
-    plt.ylabel("Inclination [deg]")
     plt.show()
 
 if __name__ == '__main__':
